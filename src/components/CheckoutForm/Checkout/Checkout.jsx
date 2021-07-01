af024523f1c45b400dbf621996b4c010d148c2b1
Paulo Carvalho <pauloej.carvalho@gmail.com>
2021-07-01 14:43:05 -0300

    feat(checkout): including Review component at Checkout

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -46,7 +46,7 @@ const Checkout = ({ cart }) => {
 
   const Form = () => activeStep === 0
     ? <AddressForm checkoutToken={checkoutToken} next={next} />
-    : <PaymentForm shippingData={shippingData} />
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
 
   const Confirmation = () => (
     <div>
--- src/components/CheckoutForm/PaymentForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm/PaymentForm.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {
+  Button,
+  Divider,
+  Typography
+} from '@material-ui/core';
+import { Elements, CardElement, ElementConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-const PaymentForm = () => {
+import Review from './Review';
+
+const PaymentForm = ({ checkoutToken }) => {
   return (
-    <div>
-      PaymentForm
-    </div>
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
   );
 }
 
