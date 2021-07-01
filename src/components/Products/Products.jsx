aacd873e1d979a2d5956113847e954d192ab33a2
Paulo Carvalho <pauloej.carvalho@gmail.com>
2021-06-30 22:01:22 -0300

    feat(commercejs): adding fetchProducts from eCommerceJs API

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,27 +2,14 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
-const products = [
-  { 
-    id: 1, 
-    name: 'DualSense ', 
-    description: 'Joystick Controller', 
-    price: '$70',
-    image: 'https://gmedia.playstation.com/is/image/SIEPDC/dualsense-hero-ps5-02-en-02jul20?$native$'
-  },
-  { 
-    id: 2, 
-    name: 'Playstation 5', 
-    description: 'Sony videogame', 
-    price: '$399',
-    image: 'https://sm.ign.com/ign_br/screenshot/default/blob_31ja.jpg'
-  },
-];
+const Products = ({ products }) => {
+  const classes = useStyles();
 
-const Products = () => {
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
           {products.map((product) => (
             <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
