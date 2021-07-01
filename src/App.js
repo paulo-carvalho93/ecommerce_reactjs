5c1e349352c70b54530376944d5b75e6d2591da0
Paulo Carvalho <pauloej.carvalho@gmail.com>
2021-06-30 22:13:43 -0300

    feat(cart): adding handleAddTocart function and update cart Icon

--- src/App.js
+++ src/App.js
@@ -4,21 +4,35 @@ import { Products, Navbar } from './components';
 
 const App = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
+    if (data) setProducts(data);
+  }
 
-    setProducts(data);
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+    if (cart) setCart(cart);
+  }
+
+  const handleAddTocart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
   }
 
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
+
+  console.log(cart);
 
   return (
     <div>
-      <Navbar />
-      <Products products={products} />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddTocart}/>
     </div>
   )
 }
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -11,7 +11,7 @@ import { AddShoppingCart } from '@material-ui/icons';
 
 import useStyles from './styles';
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
   const classes = useStyles();
 
   return (
@@ -33,7 +33,7 @@ const Product = ({ product }) => {
         />
       </CardContent>
       <CardActions disableSpacing className={classes.CardActions}>
-        <IconButton aria-label="Add to cart">
+        <IconButton aria-label="Add to cart" onClick={() => onAddToCart(product.id, 1)}>
           <AddShoppingCart />
         </IconButton>
       </CardActions>
